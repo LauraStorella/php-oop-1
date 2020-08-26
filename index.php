8d565dec639f67bb4c9d3f99854f918ba04223e3
Laura <lstorella10@gmail.com>
2020-08-26 16:14:21 +0200

    modifica proprietà e metodi

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ class UserProfile {
   public $password;
   public $login = false;
 
-  public function getLoginStatus($login)
+  public function setLoginStatus($login)
   {
     if ($login === true) {
       echo 'Welcome';
@@ -17,6 +17,11 @@ class UserProfile {
     else {
       echo 'Access denied.';
     }
+  }
+
+  public function getLoginStatus()
+  {
+    return $this->login;
   }
 }
 
@@ -28,8 +33,10 @@ $user1->surname = 'Rossi';
 $user1->age = 30;
 $user1->email = 'mrossi@gmail.com';
 $user1->password = 12345;
-$user1->login = false;
-// var_dump($user1);
+$user1->setLoginStatus(false);
+$login=$user1->getLoginStatus();
+// $user1->setLoginStatus(false);
+var_dump($user1);
 
 
 $user2 = new UserProfile();
@@ -38,8 +45,10 @@ $user2->surname = 'Bianchi';
 $user2->age = 25;
 $user2->email = 'sara@libero.it';
 $user2->password = 67890;
-$user2->login = true;
-// var_dump($user2);
+$user2->setLoginStatus(true);
+$login=$user2->getLoginStatus();
+// $user2->login = true;
+var_dump($user2);
 
 
 ?>
